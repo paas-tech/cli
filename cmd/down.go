df17817750f1212ed7804cc819077708f5133690
Noé Tarbouriech <noe.tarbouriech@etu.umontpellier.fr>
2023-07-11 20:29:58 +0200

    feat(projects): implement paastech down command

--- cmd/down.go
+++ cmd/down.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
+	"github.com/paastech-cloud/cli/internal/config"
+	"github.com/paastech-cloud/cli/pkg/project"
 	"github.com/spf13/cobra"
 )
 
@@ -11,7 +14,36 @@ var downCmd = &cobra.Command{
 	Use:     "down",
 	Short:   "Stop a deployment",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Print("Stopping deployment")
+		fmt.Println("Stopping deployment")
+
+		userCfg, err := config.LoadAuthConfig()
+		if err != nil {
+			return err
+		}
+
+		connected, err := config.IsAuthenticated(userCfg)
+		if err != nil {
+			return err
+		}
+		if !connected {
+			return errors.New("Not logged in")
+		}
+
+		projCfg, err := config.LoadProjectConfig()
+		if err != nil {
+			return err
+		}
+
+		var project project.Project
+		projCfg.UnmarshalKey("project", &project)
+
+		err = project.Down(userCfg.GetString("server"), userCfg.GetString("jwt"))
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("Project successfully stopped")
+
 		return nil
 	},
 }
--- pkg/project/deployment.go
+++ pkg/project/deployment.go
@@ -12,6 +12,7 @@ type projectDeployRequest struct {
 	EnvVars map[string]string `json:"env_vars"`
 }
 
+// Deploy a project to PaaSTech
 func (p *Project) Deploy(baseURL string, accessToken string, envVars map[string]string) error {
 	// Create JSON request body
 	request, err := json.Marshal(projectDeployRequest{
@@ -21,8 +22,36 @@ func (p *Project) Deploy(baseURL string, accessToken string, envVars map[string]
 		return err
 	}
 
-	// Create POST request to API
+	// Create PATCH request to API
 	req, err := http.NewRequest(http.MethodPatch, baseURL+"/projects/"+p.Id+"/deploy", bytes.NewReader(request))
+	if err != nil {
+		return err
+	}
+
+	// Add access token as Bearer token in headers
+	req.Header.Add("Authorization", "Bearer "+accessToken)
+	req.Header.Add("Content-Type", "application/json")
+
+	// Make request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Check if the response is an error
+	err = utils.Error(resp)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Stop a project from PaaSTech
+func (p *Project) Down(baseURL string, accessToken string) error {
+	// Create POST request to API
+	req, err := http.NewRequest(http.MethodPost, baseURL+"/projects/"+p.Id+"/stop", nil)
 	if err != nil {
 		return err
 	}
